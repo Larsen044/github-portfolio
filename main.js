7aacb425db74fc20f4699bf1b6a294f0c8a4ef51
Larsen044 <114551671+Larsen044@users.noreply.github.com>
2023-11-26 19:52:53 +0800

    New

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "UI/UX Designer", "Web Developer"],
+    typespeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+});
